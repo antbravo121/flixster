26c8b474984ef4483b621a0a8850f23fa43e3128
Anthony Bravo <antbravo121@gmail.com>
2021-02-27 18:51:30 -0500

    adding collection layout and tab bar nav.

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Anthony Bravo on 2/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
